f9877110a6262a371a4a43783fdb738c62179eb4
JoseRotsaert <jose.rotsaert@gmail.com>
2021-04-01 23:35:37 +0200

    Bounce method

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
 
-
-
 screen.listen()
 screen.onkey(key="Up", fun=r_paddle.move_up)
 screen.onkey(key="Down", fun=r_paddle.move_down)
@@ -27,6 +25,13 @@ while game_is_on:
     screen.update()
     ball.move()
 
+    # detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # detect collision with paddle
+    if ball.xcor() > 320 and ball.distance(r_paddle) < 50 or ball.xcor() < -320 and ball.distance(l_paddle) < 50 :
+        ball.bounce_x()
 
 
 screen.exitonclick()
